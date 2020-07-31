462edd2cc020294c22e52592002658eecb734946
Aaron Contreras <aaronlcaq@gmail.com>
2020-07-31 15:39:18 -0500

    Refactor neighbor_cells

--- lib/board.rb
+++ lib/board.rb
@@ -24,12 +24,12 @@ class Board
       end
     end
 
-    vertical_win = @grid.transpose.any? do |row|
+    vertical_win = @grid.transpose.any? do |column|
       perpendicular_wins.any? do |setup|
-        !row[setup[0]].empty? &&
-          row[setup[0]] == row[setup[1]] &&
-          row[setup[1]] == row[setup[2]] &&
-          row[setup[2]] == row[setup[3]]
+        !column[setup[0]].empty? &&
+          column[setup[0]] == column[setup[1]] &&
+          column[setup[1]] == column[setup[2]] &&
+          column[setup[2]] == column[setup[3]]
       end
     end
 
@@ -44,23 +44,24 @@ class Board
       row.each_index do |column_index|
         cell = @grid[row_index][column_index]
 
-        unless cell.empty?
-          search_directions.each do |vertical_travel, horizontal_travel|
-            indexes_of_cells_in_direction = [
-              [row_index + vertical_travel, column_index + horizontal_travel],
-              [row_index + 2 * vertical_travel, column_index + 2 * horizontal_travel],
-              [row_index + 3 * vertical_travel, column_index + 3 * horizontal_travel]
-            ]
-            if indexes_of_cells_in_direction.all? do |r, c|
-              r.between?(0, 5) && c.between?(0, 6)
-            end
-              if cell == @grid[indexes_of_cells_in_direction[0].first][indexes_of_cells_in_direction[0].last] &&
-                cell == @grid[indexes_of_cells_in_direction[1].first][indexes_of_cells_in_direction[1].last] &&
-                cell == @grid[indexes_of_cells_in_direction[2].first][indexes_of_cells_in_direction[2].last]
-                return true
-              end
-            end
-          end
+        next if cell.empty?
+
+        search_directions.each do |vertical_travel, horizontal_travel|
+          neighbor_indexes = [
+            [row_index + vertical_travel, column_index + horizontal_travel],
+            [row_index + 2 * vertical_travel, column_index + 2 * horizontal_travel],
+            [row_index + 3 * vertical_travel, column_index + 3 * horizontal_travel]
+          ]
+          next unless neighbor_indexes.all? do |r, c|
+                        r.between?(0, 5) && c.between?(0, 6)
+                      end
+
+          neighbor_cells = [
+            @grid[neighbor_indexes[0].first][neighbor_indexes[0].last],
+            @grid[neighbor_indexes[1].first][neighbor_indexes[1].last],
+            @grid[neighbor_indexes[2].first][neighbor_indexes[2].last]
+          ]
+          return true if neighbor_cells.all? { |neighbor| cell == neighbor }
         end
       end
     end
