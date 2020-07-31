377402a39170c57ac8e886d9ecfc7340d4efc645
Aaron Contreras <aaronlcaq@gmail.com>
2020-07-31 11:13:59 -0500

    Add code for vertical win checks

--- lib/board.rb
+++ lib/board.rb
@@ -5,20 +5,31 @@ class Board
   end
 
   def four_in_a_row?
-    horizontal_wins = [
+    perpendicular_wins = [
       [0, 1, 2, 3],
       [1, 2, 3, 4],
       [2, 3, 4, 5],
       [3, 4, 5, 6]
     ]
 
-    @grid.any? do |row|
-      horizontal_wins.any? do |setup|
+    horizontal_win = @grid.any? do |row|
+      perpendicular_wins.any? do |setup|
         !row[setup[0]].empty? &&
         row[setup[0]] == row[setup[1]] &&
         row[setup[1]] == row[setup[2]] &&
         row[setup[2]] == row[setup[3]]
-      end 
+      end
     end
+
+    vertical_win = @grid.transpose.any? do |row|
+      perpendicular_wins.any? do |setup|
+        !row[setup[0]].empty? &&
+        row[setup[0]] == row[setup[1]] &&
+        row[setup[1]] == row[setup[2]] &&
+        row[setup[2]] == row[setup[3]]
+      end
+    end
+
+    horizontal_win || vertical_win
   end
 end
