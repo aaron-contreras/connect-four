8517181aa9da8d03dd5e3307764fe206efeb2d16
Aaron Contreras <aaronlcaq@gmail.com>
2020-08-02 17:24:05 -0500

    Add tests for invalid names

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,17 +5,33 @@ describe Game do
   let(:game) { described_class.new }
 
   describe '#start_game' do
+    context 'when given an invalid name' do
+      it 'asks for a name until valid' do
+        # Arrange
+        allow(game).to receive(:print)
+        allow(game).to receive(:gets).and_return('Aaron1', '', 'Aaron', 'Chad')
+        allow(game).to receive(:play_turns)
+
+        # Assert
+        expect(game).to receive(:gets).exactly(4).times
+
+        # Act
+        game.start_game
+      end
+    end
+
     it 'creates two players' do
       # Arrange
       allow(game).to receive(:print).twice
       allow(game).to receive(:gets).and_return('Aaron', 'Chad')
+      allow(game).to receive(:play_turns)
 
       # Act
       game.start_game
 
-      players = game.instance_variable_get(:@players)
 
       # Assert
+      players = game.instance_variable_get(:@players)
       expect(players.size).to eq 2
     end
 
