4e3f1344f285e5b005e01518f081add868ed365e
Aaron Contreras <aaronlcaq@gmail.com>
2020-08-05 16:56:16 -0500

    Correct discs for repl.it

--- lib/board.rb
+++ lib/board.rb
@@ -104,7 +104,7 @@ class Board
   end
 
   def formatted_row(row)
-    row = @grid[row].map { |cell| cell == '' ? '  ' : cell }
+    row = @grid[row].map { |cell| cell == '' ? '  ' : "#{cell} " }
 
     " #{row[0]} | #{row[1]} | #{row[2]} | #{row[3]} | #{row[4]} | #{row[5]} | #{row[6]} "
   end
