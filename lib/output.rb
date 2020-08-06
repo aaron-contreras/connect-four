c68ff07cce22a58c86195c3abacff1755b942c39
Aaron Contreras <aaronlcaq@gmail.com>
2020-08-06 12:19:18 -0500

    Abstract output formatting into a module

---
+++ lib/output.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Output
+  RED_DISC = "\u{1f534}"
+  BLUE_DISC = "\u{1f535}"
+
+  COLORS = {
+    red: "\e[31m",
+    green: "\e[32m",
+    yellow: "\e[33m",
+    blue: "\e[34m"
+  }
+
+  def clear_screen
+    print "\e[2J\e[H"
+  end
+
+  def clear_line_above
+    print "\e[1A\e[K"
+  end
+end
