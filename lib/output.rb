02b361ad34d6e8e8b87d8e45de02642ce424cc43
Aaron Contreras <aaronlcaq@gmail.com>
2020-08-07 10:17:53 -0500

    Add Board constants

--- lib/output.rb
+++ lib/output.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# rubocop: disable Layout/LineLength
+
+# Constants and methods for prettier terminal output
 module Output
   RED_DISC = "\u{1f534}"
   BLUE_DISC = "\u{1f535}"
@@ -9,7 +12,7 @@ module Output
     green: "\e[32m",
     yellow: "\e[33m",
     blue: "\e[34m"
-  }
+  }.freeze
 
   CLEAR_FORMATTING = "\e[0m"
 
@@ -20,4 +23,34 @@ module Output
   def clear_line_above
     print "\e[1A\e[K"
   end
+
+  # Board related
+  GRID_WIDTH = 34
+  COLUMNS = 7
+  ROWS = 6
+  SPACE_BETWEEN_FEET = GRID_WIDTH - 2
+
+  SQUARE = "\e[47m  \e[0m"
+
+  CIRCLED_LETTERS = {
+    a: "\u24b6",
+    b: "\u24b7",
+    c: "\u24b8",
+    d: "\u24b9",
+    e: "\u24ba",
+    f: "\u24bb",
+    g: "\u24bc"
+  }.freeze
+
+  COLUMN_HEADINGS = "#{SQUARE}| #{COLORS[:green]}#{CIRCLED_LETTERS.map { |_k, v| v }.join(' ' * 4)}#{CLEAR_FORMATTING}  |#{SQUARE}"
+
+  COLUMN_HEADING_DIVIDER = "#{SQUARE}|#{'=' * GRID_WIDTH}|#{SQUARE}"
+  ROW_DIVIDER = "#{SQUARE}|#{COLUMNS.times.map { '----' }.join('+')}|#{SQUARE}"
+  BOARD_FEET = SQUARE * 3
+
+  def format_row(row)
+    row = grid[row].map { |cell| cell == '' ? '  ' : "#{cell} " }.join(' | ')
+    "#{SQUARE}| #{row} |#{SQUARE}"
+  end
 end
+# rubocop: enable Layout/LineLength
